df066cd844199f185e82435e9fc1282a5edfe78f
Bruno Trindade <b.l.trindade@outlook.com>
2019-05-04 12:51:10 -0300

    Alteração aluno 8

--- src/main/java/br/com/senaigo/chamada/model/Aluno8.java
+++ src/main/java/br/com/senaigo/chamada/model/Aluno8.java
@@ -4,7 +4,7 @@ public class Aluno8 implements RespondeChamada {
     
     //Escreva seu nome seguido de presente. Exemplo: return "Fulano - presente";
     public String responderChamada() {
-        return "Aluno8 - ...";
+        return "Aluno8 - Bruno Leonardo Trindade Costa";
     }
     
 }
