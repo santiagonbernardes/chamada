736821f82994b36201a66d1024000f738850d5eb
Fernando <serve.barcelos@gmail.com>
2019-05-04 12:48:36 -0300

    Alteração da chamada

--- src/main/java/br/com/senaigo/chamada/model/Aluno2.java
+++ src/main/java/br/com/senaigo/chamada/model/Aluno2.java
@@ -4,7 +4,7 @@ public class Aluno2 implements RespondeChamada {
     
     //Escreva seu nome seguido de presente. Exemplo: return "Fulano - presente";
     public String responderChamada() {
-        return "Aluno2 - ...";
+        return "Aluno2 - Fernando Barcelos da Costa";
     }
     
 }
